363800688fee0a46b6e6aaf77f23250bd11dd5fd
ngoc anh <anhcn14103@gmail.com>
2023-12-08 10:07:37 +0700

    Add project files.

---
+++ ChuNgocAnh_211210503/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using ChuNgocAnh_211210503.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ChuNgocAnh_211210503.Controllers
+{
+	public class HomeController : Controller
+	{
+		private QLHangHoaContext db;
+		public HomeController(QLHangHoaContext context)
+		{
+			db = context;
+		}
+
+		public IActionResult Index()
+		{
+			IEnumerable<HangHoa> list_hh = db.HangHoas.Where(hh => hh.Gia > 100).ToList();
+			return View("ChuNgocAnh_mainContent", list_hh);
+		}
+
+
+		public IActionResult MatHangByLoaiHangID(int mid)
+		{
+			var lh = db.HangHoas.Where(l => l.MaLoai == mid).ToList();
+			return PartialView("TableHangHoa", lh);
+		}
+
+		public IActionResult AddToCart()
+		{
+			ViewBag.CategoryId = new SelectList(db.LoaiHangs, "MaLoai", "TenLoai");
+			return View();
+		}
+
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult AddToCart(HangHoa hh)
+		{
+			if (!ModelState.IsValid)
+			{
+				db.HangHoas.Add(hh);
+				db.SaveChanges();
+
+				return RedirectToAction("Index");
+			}
+
+			return View("AddToCart");
+		}
+
+	}
+}
---
+++ ChuNgocAnh_211210503/ViewComponents/NavViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ChuNgocAnh_211210503.Models;
+
+namespace ChuNgocAnh_211210503.ViewComponents
+{
+	public class NavViewComponent : ViewComponent
+	{
+		QLHangHoaContext db;
+		List<LoaiHang> majors;
+		public NavViewComponent(QLHangHoaContext _context)
+		{
+			db = _context;
+			majors = db.LoaiHangs.ToList();
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View("RenderNav", majors);
+		}
+	}
+}
